592832e7bd0d5a42300d39282da7aa1514e3858e
Jackson Chan <jacksonsiachan@gmail.com>
2020-04-02 19:04:55 +0800

    List volumes and snapshots. Naive implementation of snapshot command

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -7,16 +7,76 @@ ec2 = session.resource('ec2')
 def filter_instances(project):
      instances = []
      if project:
-        filters = [{'Name': 'tag:project', 'Values':[project]}] 
+        filters = [{'Name': 'tag:Project', 'Values':[project]}] 
         instances = ec2.instances.filter(Filters=filters)
      else:
         instances = ec2.instances.all() 
      return instances
 
 @click.group()
+def cli(): #Parent group
+   """Shotty manages snapshots"""
+
+@cli.group('snapshots')  
+def snapshots():
+   """Commands for snapshots"""
+
+@snapshots.command('list')
+@click.option('--project', default=None, help="Only snapshots for project (tag Project:<name>)")  
+def list_snapshots(project):
+    "List EC2 snapshots"
+    instances = filter_instances(project)
+    for i in instances:
+       for v in i.volumes.all():
+           for s in v.snapshots.all():
+               print(", ".join((
+                  s.id,
+                  v.id,
+                  i.id,
+                  s.state,
+                  s.progress,
+                  s.start_time.strftime("%c")
+               )))
+    return            
+
+
+@cli.group('volumes')
+def volumes():
+   """Commands for volumes"""
+
+@volumes.command('list')     
+@click.option('--project', default=None, help="Only volumes for project (tag Project:<name>)")  
+def list_volumes(project):
+   "List EC2 volumes"
+   instances = filter_instances(project)
+   for i in instances:
+      for v in i.volumes.all():
+         print(", ".join((
+               v.id,
+               i.id,
+               v.state,
+               str(v.size) + "GiB",
+               v.encrypted and "Encrypted" or "Not Encrypted"
+         ))) #Tuple of attributes
+   return       
+
+@cli.group('instances')
 def instances():
     """Commands for instances"""
 
+@instances.command('snapshot', help="Create snapshots of all volumes.")
+@click.option('--project', default=None, help="Only instances for project (tag Project:<name>)")
+def create_snapshots(project):
+   "Create Snapshots of volume."
+   instances = filter_instances(project)
+   
+   for i in instances:
+       for v in i.volumes.all():
+          print("Creating snapshot of {0}".format(v.id))
+          v.create_snapshot(Description="Created by SnapshotAlyzer 30000")
+
+   return       
+               
 
 @instances.command('list')     
 @click.option('--project', default=None, help="Only instances for project (tag Project:<name>)")
@@ -26,7 +86,7 @@ def list_instances(project):
 
     for i in instances:
         tags = { t['Key'] :  t['Value'] for t in i.tags or [] }
-        print(', '.join((i.id, i.instance_type, i.placement['AvailabilityZone'], i.state['Name'], i.public_dns_name, tags.get('project', '<no project>'))))
+        print(', '.join((i.id, i.instance_type, i.placement['AvailabilityZone'], i.state['Name'], i.public_dns_name, tags.get('Project', '<no project>'))))
     return
 
 @instances.command('stop')
@@ -52,7 +112,7 @@ def start_instances(project):
      return
 
 if __name__ == '__main__':
-    instances()
+    cli()
   
   
   
